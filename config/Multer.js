37ffe5a170d0ec524f014bd77edac7042b843f2e
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-19 07:14:33 +0100

    multer up and running

--- config/Multer.js
+++ config/Multer.js
@@ -1,27 +1,35 @@
 const multer = require("multer");
+const path = require("path")
+console.log(path)
 
-const storage = multer.diskStorage({
-	destination: (req, res, cb) => {
-		cb(null, "./uploads");
-	},
-
-	filename: (req, file, cb) => {
-		cb(null, file.originalname);
-	},
+const Storage = multer.diskStorage({
+  destination: (
+    req,
+    file,
+    cb
+  ) => {
+    cb(null, path.join(__dirname, "../Uploads"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
 });
-
-const filefilter = (req, file, cb) => {
-	if (
-		file.minetype === "/jpg" ||
-		file.minetype === "/png" ||
-		file.minetype === "/jpeg"
-	) {
-		cb(null, true);
-	}
-};
+// const filefilter = (req, file, cb) => {
+// 	if (
+// 		file.minetype === "/jpg" ||
+// 		file.minetype === "/png" ||
+// 		file.minetype === "/jpeg"
+// 	) {
+// 		cb(null, true);
+// 	}
+// };
 
 const imageUploader = multer({
-	storage: storage,
+	storage: Storage,
 	// fileFilter: filefilter,
 	// limits: {
 	// 	fileSize: 1024 * 1024 * 2,
